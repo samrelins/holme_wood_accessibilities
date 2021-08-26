ecd300affca7a660d7e99ca4b782a8d14081f88b
samrelins <samrelins@gmail.com>
2021-08-26 16:34:37 +0100

    added aggregation methods to build_accessbility_table

--- src/journey_time_helpers.py
+++ src/journey_time_helpers.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 
 ### Change the DATA_DIR variable to relevant directory ###
@@ -7,11 +8,12 @@ DATA_DIR = '/Users/samrelins/Documents/LIDA/transport_proj/data'
 ### Either name data "jt_data_joined.csv or change JT_CSV_NAME ###
 JT_CSV_NAME = "jt_data_joined.csv"
 ###
-def build_accessibility_table(lsoas=None):
+def build_accessibility_table(lsoas, agg_method=None):
     """
     function to return accessibility statistics given LSOA codes
 
     :param lsoas: (string / list / none) Required LSOA codes
+    :param agg_method: (string) Aggregation method if multiple LSOAs passed
     :return: (DataFrame) Dataframe of accessibility stats for requested LSOA(s)
     """
 
@@ -20,13 +22,26 @@ def build_accessibility_table(lsoas=None):
     full_jt_data = pd.read_csv(jt_path, low_memory=False)
 
     # read in lsoa(s) and adjust to list if only an individual value
-    if lsoas is not None:
-        if not type(lsoas) == list:
-            lsoas = [lsoas]
-        in_specified_lsoas = full_jt_data.LSOA_code.isin(lsoas)
-        lsoas_jt_data = full_jt_data[in_specified_lsoas]
+    if type(lsoas) == list:
+        if agg_method is None:
+            raise ValueError("Must specify agg_method for multiple LSOAs")
     else:
-        lsoas_jt_data = full_jt_data
+        lsoas = [lsoas]
+
+    def pct10(x):
+        return np.percentile(x, 10, interpolation="nearest")
+    if agg_method == "pct10":
+        agg_method = pct10
+
+    def pct90(x):
+        return np.percentile(x, 90, interpolation="nearest")
+    if agg_method == "pct90":
+        agg_method = pct90
+
+    print(agg_method)
+
+    in_specified_lsoas = full_jt_data.LSOA_code.isin(lsoas)
+    lsoas_jt_data = full_jt_data[in_specified_lsoas]
 
     # dictionaries storing variable names from dft dataframes
     service_names = {
@@ -69,10 +84,13 @@ def build_accessibility_table(lsoas=None):
                       for observation in observation_names.keys()]
 
     # Extract required variables from dft data
-    mean_values = (lsoas_jt_data[variable_names]
-                   .mean().values
-                   .reshape(30, 5).T)
-    data = dict(zip(observation_names.values(), mean_values))
+    table_data = lsoas_jt_data[variable_names]
+    if agg_method is not None:
+        table_data = table_data.agg(agg_method).values
+    else:
+        table_data = table_data.values
+    table_data = table_data.reshape(30, 5).T
+    dataframe_dict = dict(zip(observation_names.values(), table_data))
 
     # convert data to dataframe and return
-    return pd.DataFrame(data, index=index)
+    return pd.DataFrame(dataframe_dict, index=index)
