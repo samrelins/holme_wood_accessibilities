034ab056c7db75f7b04997801fb8bd31f7ed2882
samrelins <samrelins@gmail.com>
2021-09-02 15:16:49 +0100

    updated visual analysis helper

--- src/journey_time_helpers.py
+++ src/journey_time_helpers.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+import plotly.express as px
 
 ### Change the DATA_DIR variable to relevant directory ###
 DATA_DIR = '/Users/samrelins/Documents/LIDA/transport_proj/data'
@@ -8,6 +9,37 @@ DATA_DIR = '/Users/samrelins/Documents/LIDA/transport_proj/data'
 ### Either name data "jt_data_joined.csv or change JT_CSV_NAME ###
 JT_CSV_NAME = "jt_data_joined.csv"
 ###
+
+JT_PATH = os.path.join(DATA_DIR, JT_CSV_NAME)
+FULL_JT_DATA = pd.read_csv(JT_PATH, low_memory=False)
+
+SERVICE_NAMES = {
+    "100Emp": "Employment Sites - Small",
+    "500Emp": "Employment Sites - Medium",
+    "5000Emp": "Employment Sites - Large",
+    "PS": "Primary Schools",
+    "SS": "Secondary Schools",
+    "FE": "Further Education",
+    "GP": "GP",
+    "Hosp": "Hospitals",
+    "Food": "Food Stores",
+    "Town": "Town Centres",
+}
+
+MODE_NAMES = {
+    "PT": "Public Transport / Walk",
+    "Cyc": "Cycle",
+    "Car": "Car"
+}
+
+OBSERVATION_NAMES = {
+    "t": ("", "Journey Time (min)"),
+    "15n": ("Origin Accessibility", "n Services Within 15 min"),
+    "30n": ("Origin Accessibility", "n Services Within 30 min"),
+    "15pct": ("Destination Accessibility", "% Service Users Within 15 min"),
+    "30pct": ("Destination Accessibility", "% Service Users Within 30 min"),
+}
+
 def build_accessibility_table(lsoas, agg_method=None):
     """
     function to return accessibility statistics given LSOA codes
@@ -18,8 +50,6 @@ def build_accessibility_table(lsoas, agg_method=None):
     """
 
     # load journey time data as outputted from clean_and_merge_jt_data.py
-    jt_path = os.path.join(DATA_DIR, JT_CSV_NAME)
-    full_jt_data = pd.read_csv(jt_path, low_memory=False)
 
     # read in lsoa(s) and adjust to list if only an individual value
     if type(lsoas) == list:
@@ -38,48 +68,22 @@ def build_accessibility_table(lsoas, agg_method=None):
     if agg_method == "pct90":
         agg_method = pct90
 
-    in_specified_lsoas = full_jt_data.LSOA_code.isin(lsoas)
-    lsoas_jt_data = full_jt_data[in_specified_lsoas]
+    in_specified_lsoas = FULL_JT_DATA.LSOA_code.isin(lsoas)
+    lsoas_jt_data = FULL_JT_DATA[in_specified_lsoas]
 
     # dictionaries storing variable names from dft dataframes
-    service_names = {
-        "100Emp": "Employment Sites - Small",
-        "500Emp": "Employment Sites - Medium",
-        "5000Emp": "Employment Sites - Large",
-        "PS": "Primary Schools",
-        "SS": "Secondary Schools",
-        "FE": "Further Education",
-        "GP": "GP",
-        "Hosp": "Hospitals",
-        "Food": "Food Stores",
-        "Town": "Town Centres",
-    }
-
-    mode_names = {
-        "PT": "Public Transport / Walk",
-        "Cyc": "Cycle",
-        "Car": "Car"
-    }
-
-    observation_names = {
-        "t": ("", "Journey Time (min)"),
-        "15n": ("Origin Accessibility", "n Services Within 15 min"),
-        "30n": ("Origin Accessibility", "n Services Within 30 min"),
-        "15pct": ("Destination Accessibility", "% Service Users Within 15 min"),
-        "30pct": ("Destination Accessibility", "% Service Users Within 30 min"),
-    }
 
     # create list of name tuples for dataframe MultiIndex
     index_tuples = [(service, mode)
-                    for service in service_names.values()
-                    for mode in mode_names.values()]
+                    for service in SERVICE_NAMES.values()
+                    for mode in MODE_NAMES.values()]
     index = pd.MultiIndex.from_tuples(index_tuples, names=("Service", "Mode"))
 
     # Create a list of the required variable names
     variable_names = [service + mode + observation
-                      for service in service_names.keys()
-                      for mode in mode_names.keys()
-                      for observation in observation_names.keys()]
+                      for service in SERVICE_NAMES.keys()
+                      for mode in MODE_NAMES.keys()
+                      for observation in OBSERVATION_NAMES.keys()]
 
     # Extract required variables from dft data
     table_data = lsoas_jt_data[variable_names]
@@ -88,27 +92,27 @@ def build_accessibility_table(lsoas, agg_method=None):
     else:
         table_data = table_data.values
     table_data = table_data.reshape(30, 5).T
-    dataframe_dict = dict(zip(observation_names.values(), table_data))
+    dataframe_dict = dict(zip(OBSERVATION_NAMES.values(), table_data))
 
     # convert data to dataframe and return
     return pd.DataFrame(dataframe_dict, index=index)
 
 
 def compare_destination_features(
-        lsoas_1, lsoas_2, service, observation, group_names, title=""
-):
+        lsoa_groups, group_names, service, observation):
     columns = [service + transport_mode + observation
                for transport_mode in [ "PT", "Cyc", "Car"]]
 
-    group_1 = full_jt_data.LSOA_code.isin(lsoas_1)
-    group_1_data = full_jt_data[group_1][columns]
-    group_1_data["location"] = group_names[0]
+    joined_data = None
+    for lsoa_group, group_name in zip(lsoa_groups, group_names):
+        is_in_group = FULL_JT_DATA.LSOA_code.isin(lsoa_group)
+        group_data = FULL_JT_DATA[is_in_group][columns]
+        group_data["location"] = group_name
 
-    group_2 = full_jt_data.LSOA_code.isin(lsoas_2)
-    group_2_data = full_jt_data[group_2][columns]
-    group_2_data["location"] = group_names[1]
-
-    joined_data = group_1_data.append(group_2_data)
+        if joined_data is None:
+            joined_data = group_data
+        else:
+            joined_data = joined_data.append(group_data)
 
     y_axis_names = {
         "t": "Time (min)",
@@ -124,5 +128,9 @@ def compare_destination_features(
         labels=dict(variable="Mode of Transport",
                     value=y_axis_names[observation])
     )
+
+    title = (SERVICE_NAMES[service]
+             + ": "
+             + OBSERVATION_NAMES[observation][1])
     plot.update_layout(title=dict(text=title, x=0.5))
     return plot
